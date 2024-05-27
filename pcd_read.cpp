71ed1e8beb510f8a2f8aa991db9244ecb1df3671
Ajaay Chandrasekaran <ajaay@umich.edu>
2024-05-27 02:34:40 -0400

    Read parameters from file

--- pcd_read.cpp
+++ pcd_read.cpp
@@ -1,3 +1,4 @@
+#include <fstream>
 #include <iostream>
 #include <pcl/common/io.h>
 #include <pcl/features/normal_3d.h>
@@ -17,6 +18,60 @@
 using namespace std::chrono_literals;
 
 namespace cloud_helpers {
+
+void readParamsFromFile(const std::string &parameters_file_name,
+                        int &filtering_meank, double &filtering_stddevmulthresh,
+                        double &voxelization_leaf_size_x,
+                        double &voxelization_leaf_size_y,
+                        double &voxelization_leaf_size_z,
+                        unsigned &segmentation_k_search_count,
+                        unsigned &segmentation_min_cluster_size,
+                        unsigned &segmentation_max_cluster_size,
+                        unsigned &segmentation_num_neighbors,
+                        double &segmentation_smoothness_threshold,
+                        double &segmentation_curvature_threshold) {
+  std::ifstream params_file(parameters_file_name);
+  std::string param_name;
+
+  params_file >> param_name >> filtering_meank;
+  params_file >> param_name >> filtering_stddevmulthresh;
+  params_file >> param_name >> voxelization_leaf_size_x;
+  params_file >> param_name >> voxelization_leaf_size_y;
+  params_file >> param_name >> voxelization_leaf_size_z;
+  params_file >> param_name >> segmentation_k_search_count;
+  params_file >> param_name >> segmentation_min_cluster_size;
+  params_file >> param_name >> segmentation_max_cluster_size;
+  params_file >> param_name >> segmentation_num_neighbors;
+  params_file >> param_name >> segmentation_smoothness_threshold;
+  params_file >> param_name >> segmentation_curvature_threshold;
+
+  params_file.close();
+
+  std::cout << "Parameters Used:" << std::endl;
+  std::cout << "filtering_meank: " << filtering_meank << std::endl; //{30};
+  std::cout << "filtering_stddevmulthresh: " << filtering_stddevmulthresh
+            << std::endl; //{1.0f};
+  std::cout << "voxelization_leaf_size_x: " << voxelization_leaf_size_x
+            << std::endl; //{0.3f};
+  std::cout << "voxelization_leaf_size_y: " << voxelization_leaf_size_y
+            << std::endl; //{0.3f};
+  std::cout << "voxelization_leaf_size_z: " << voxelization_leaf_size_z
+            << std::endl; //{0.3f};
+  std::cout << "segmentation_k_search_count: " << segmentation_k_search_count
+            << std::endl; //{50};
+  std::cout << "segmentation_min_cluster_size: "
+            << segmentation_min_cluster_size << std::endl; //{300};
+  std::cout << "segmentation_max_cluster_size: "
+            << segmentation_max_cluster_size << std::endl; //{1000000};
+  std::cout << "segmentation_num_neighbors: " << segmentation_num_neighbors
+            << std::endl; //{30};
+  std::cout << "segmentation_smoothness_threshold: "
+            << segmentation_smoothness_threshold
+            << std::endl; //{3.0 / 180.0 * M_PI};
+  std::cout << "segmentation_curvature_threshold: "
+            << segmentation_curvature_threshold << std::endl; //{1.0};
+}
+
 void printCloudInfo(const std::string &cloud_name,
                     pcl::PointCloud<pcl::PointXYZ>::ConstPtr cloud) {
   std::cout << cloud_name << " has: " << cloud->width * cloud->height
@@ -315,21 +370,30 @@ private:
 };
 
 int main() {
-  std::string file_name = "scans.pcd";
-  int filtering_meank{30};
-  double filtering_stddevmulthresh{1.0f};
-  double voxelization_leaf_size_x{0.3f};
-  double voxelization_leaf_size_y{0.3f};
-  double voxelization_leaf_size_z{0.3f};
+  std::string cloud_file_name = "scans.pcd";
+  std::string parameters_file_name = "parameters.txt";
 
-  unsigned segmentation_k_search_count{50};
-  unsigned segmentation_min_cluster_size{300};
-  unsigned segmentation_max_cluster_size{1000000};
-  unsigned segmentation_num_neighbors{30};
-  double segmentation_smoothness_threshold{3.0 / 180.0 * M_PI};
-  double segmentation_curvature_threshold{1.0};
+  int filtering_meank;                      //{30};
+  double filtering_stddevmulthresh;         //{1.0f};
+  double voxelization_leaf_size_x;          //{0.3f};
+  double voxelization_leaf_size_y;          //{0.3f};
+  double voxelization_leaf_size_z;          //{0.3f};
+  unsigned segmentation_k_search_count;     //{50};
+  unsigned segmentation_min_cluster_size;   //{300};
+  unsigned segmentation_max_cluster_size;   //{1000000};
+  unsigned segmentation_num_neighbors;      //{30};
+  double segmentation_smoothness_threshold; //{3.0 / 180.0 * M_PI};
+  double segmentation_curvature_threshold;  //{1.0};
 
-  PointCloudProcessorEngine eng{file_name,
+  cloud_helpers::readParamsFromFile(
+      parameters_file_name, filtering_meank, filtering_stddevmulthresh,
+      voxelization_leaf_size_x, voxelization_leaf_size_y,
+      voxelization_leaf_size_z, segmentation_k_search_count,
+      segmentation_min_cluster_size, segmentation_max_cluster_size,
+      segmentation_num_neighbors, segmentation_smoothness_threshold,
+      segmentation_curvature_threshold);
+
+  PointCloudProcessorEngine eng{cloud_file_name,
                                 filtering_meank,
                                 filtering_stddevmulthresh,
                                 voxelization_leaf_size_x,
@@ -389,5 +453,5 @@ int main() {
 
   visualization_manager.runVisualization();
 
-  return (0);
+  return 0;
 }
