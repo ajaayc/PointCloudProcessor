cf2d06f3b324e9dea3aae90f0a406ee9cd9986a1
Ajaay Chandrasekaran <ajaay@umich.edu>
2024-05-29 02:31:51 -0400

    Initialize ros_pcl_visualizer package

---
+++ ros_pcl_visualizer/src/point_cloud_subscriber_node.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+#include "cloud_helpers.hpp"
+#include "point_cloud_visualization_manager.hpp"
+#include "point_cloud_processor_engine.hpp"
+
+int main(int argc, char ** argv)
+{
+  (void) argc;
+  (void) argv;
+
+  std::cout << "hello world ros_pcl_visualizer package\n";
+  std::cout << "Reached here\n";
+  return 0;
+}
