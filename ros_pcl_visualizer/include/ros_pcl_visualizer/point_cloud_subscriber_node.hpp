90bdc862b4e924bd15c830603ba66cf866c99a60
Ajaay Chandrasekaran <ajaay@umich.edu>
2024-05-30 00:08:01 -0400

    Implement Point Cloud Subscriber Callback

--- ros_pcl_visualizer/include/ros_pcl_visualizer/point_cloud_subscriber_node.hpp
+++ ros_pcl_visualizer/include/ros_pcl_visualizer/point_cloud_subscriber_node.hpp
@@ -3,17 +3,48 @@
 
 #include "rclcpp/rclcpp.hpp"
 #include "sensor_msgs/msg/point_cloud2.hpp"
+#include <pcl/conversions.h>
+#include <pcl/point_cloud.h>
+#include <pcl_conversions/pcl_conversions.h>
+
+#include "cloud_helpers.hpp"
+#include "point_cloud_processor_engine.hpp"
+#include "point_cloud_visualization_manager.hpp"
 
 using std::placeholders::_1;
 
 class PointCloudSubscriberNode : public rclcpp::Node {
 public:
   PointCloudSubscriberNode(
+      int filtering_meank,                      //{30};
+      double filtering_stddevmulthresh,         //{1.0f};
+      double voxelization_leaf_size_x,          //{0.3f};
+      double voxelization_leaf_size_y,          //{0.3f};
+      double voxelization_leaf_size_z,          //{0.3f};
+      unsigned segmentation_k_search_count,     //{50};
+      unsigned segmentation_min_cluster_size,   //{300};
+      unsigned segmentation_max_cluster_size,   //{1000000};
+      unsigned segmentation_num_neighbors,      //{30};
+      double segmentation_smoothness_threshold, //{3.0 / 180.0 * M_PI};
+      double segmentation_curvature_threshold,  //{1.0};
       const rclcpp::NodeOptions &options = rclcpp::NodeOptions())
       : Node("point_cloud_subscriber", options),
-        input_topic_name{"/tof_camera/xyz"}, output_topic_name{
-                                                 "/processed_cloud"} {
-    publisher = this->create_publisher<sensor_msgs::msg::PointCloud2>(output_topic_name, 2);
+        input_topic_name{"/tof_camera/xyz"},
+        output_topic_name{"/processed_cloud"},
+        eng{filtering_meank,                   //{30};
+            filtering_stddevmulthresh,         //{1.0f};
+            voxelization_leaf_size_x,          //{0.3f};
+            voxelization_leaf_size_y,          //{0.3f};
+            voxelization_leaf_size_z,          //{0.3f};
+            segmentation_k_search_count,       //{50};
+            segmentation_min_cluster_size,     //{300};
+            segmentation_max_cluster_size,     //{1000000};
+            segmentation_num_neighbors,        //{30};
+            segmentation_smoothness_threshold, //{3.0 / 180.0 * M_PI};
+            segmentation_curvature_threshold}  //{1.0};} {
+  {
+    publisher = this->create_publisher<sensor_msgs::msg::PointCloud2>(
+        output_topic_name, 2);
     subscriber = this->create_subscription<sensor_msgs::msg::PointCloud2>(
         input_topic_name, 10,
         std::bind(&PointCloudSubscriberNode::pointCloudTopicCallback, this,
@@ -23,8 +54,66 @@ public:
 protected:
   void
   pointCloudTopicCallback(const sensor_msgs::msg::PointCloud2::SharedPtr msg) {
+
+    std::cout << "Entering callback fn" << std::endl;
+
+    pcl::PointCloud<pcl::PointXYZ>::Ptr cloud1(
+        new pcl::PointCloud<pcl::PointXYZ>);
+    pcl::PCLPointCloud2::Ptr cloud2(new pcl::PCLPointCloud2());
+
+    // ROS2 Pointcloud2 to PCL Pointcloud2
+    pcl_conversions::moveToPCL(*msg, *cloud2);
+
+    // PCL Pointcloud2 to PCL Pointcloud
+    pcl::fromPCLPointCloud2(*cloud2, *cloud1);
+
+    eng.setOriginalCloud(cloud1);
+
+    cloud_helpers::printCloudInfo(eng.getOriginalCloudName(),
+                                  eng.getOriginalCloud());
+
+    eng.filterOriginalCloud();
+
+    cloud_helpers::printCloudInfo(eng.getFilteredCloudName(),
+                                  eng.getFilteredCloud());
+
+    eng.voxelizeFilteredCloud();
+
+    cloud_helpers::printCloudInfo(eng.getVoxelizedCloudName(),
+                                  eng.getVoxelizedCloud());
+
+    eng.segmentVoxelizedCloud();
+
+    cloud_helpers::printSegmentedCloudInfo(eng.getSegmentedCloudName(),
+                                           eng.getSegmentedCloudClusters(),
+                                           eng.getSegmentedCloud());
+
+    pcl::PointCloud<pcl::PointXYZRGB>::ConstPtr segmented_cloud =
+        eng.getSegmentedCloud();
+
+    // cloud_helpers::printCloudPoints(segmented_cloud);
+
+    pcl::PCLPointCloud2::Ptr segmented_cloud2(new pcl::PCLPointCloud2());
+
+    pcl::toPCLPointCloud2(*segmented_cloud, *segmented_cloud2);
+
+    // std::cout << "segmented_cloud2 width: " << segmented_cloud2->width
+    //           << std::endl;
+    // std::cout << "segmented_cloud2 height: " << segmented_cloud2->height
+    //           << std::endl;
+
+    // Convert to ros message
     sensor_msgs::msg::PointCloud2 cloud_out;
-    cloud_out = *msg;
+    pcl_conversions::moveFromPCL(*segmented_cloud2, cloud_out);
+
+    // std::cout << "cloud_out.width: " << cloud_out.width << std::endl;
+
+    // std::cout << "cloud_out.height: " << cloud_out.height << std::endl;
+
+    // std::cout << "cloud_out.is_dense: " << cloud_out.is_dense << std::endl;
+
+    // For debugging
+    // cloud_out = *msg;
     publisher->publish(cloud_out);
     return;
   }
@@ -35,6 +124,8 @@ protected:
   // ROS 2 publisher
   rclcpp::Publisher<sensor_msgs::msg::PointCloud2>::SharedPtr publisher;
   std::string output_topic_name;
+
+  PointCloudProcessorEngine eng;
 };
 
 #endif // _POINT_CLOUD_SUBSCRIBER_NODE_HPP_
--- ros_pcl_visualizer/src/point_cloud_subscriber_node.cpp
+++ ros_pcl_visualizer/src/point_cloud_subscriber_node.cpp
@@ -1,21 +1,45 @@
 #include <iostream>
 
-#include "cloud_helpers.hpp"
-#include "point_cloud_visualization_manager.hpp"
-#include "point_cloud_processor_engine.hpp"
-
+#include "point_cloud_subscriber_node.hpp"
 #include "rclcpp/rclcpp.hpp"
 #include "std_msgs/msg/string.hpp"
-#include "point_cloud_subscriber_node.hpp"
 
-int main(int argc, char ** argv)
-{
+
+int main(int argc, char **argv) {
   std::cout << "hello world ros_pcl_visualizer package\n";
   std::cout << "Reached here\n";
 
-  //const rclcpp::NodeOptions options;
+   std::string parameters_file_name = "parameters.txt";
+
+
+  int filtering_meank;                      //{30};
+  double filtering_stddevmulthresh;         //{1.0f};
+  double voxelization_leaf_size_x;          //{0.3f};
+  double voxelization_leaf_size_y;          //{0.3f};
+  double voxelization_leaf_size_z;          //{0.3f};
+  unsigned segmentation_k_search_count;     //{50};
+  unsigned segmentation_min_cluster_size;   //{300};
+  unsigned segmentation_max_cluster_size;   //{1000000};
+  unsigned segmentation_num_neighbors;      //{30};
+  double segmentation_smoothness_threshold; //{3.0 / 180.0 * M_PI};
+  double segmentation_curvature_threshold;  //{1.0};
+
+  cloud_helpers::readParamsFromFile(
+      parameters_file_name, filtering_meank, filtering_stddevmulthresh,
+      voxelization_leaf_size_x, voxelization_leaf_size_y,
+      voxelization_leaf_size_z, segmentation_k_search_count,
+      segmentation_min_cluster_size, segmentation_max_cluster_size,
+      segmentation_num_neighbors, segmentation_smoothness_threshold,
+      segmentation_curvature_threshold);
+
+  // const rclcpp::NodeOptions options;
   rclcpp::init(argc, argv);
-  rclcpp::spin(std::make_shared<PointCloudSubscriberNode>());
+  rclcpp::spin(std::make_shared<PointCloudSubscriberNode>(
+      filtering_meank, filtering_stddevmulthresh, voxelization_leaf_size_x,
+      voxelization_leaf_size_y, voxelization_leaf_size_z,
+      segmentation_k_search_count, segmentation_min_cluster_size,
+      segmentation_max_cluster_size, segmentation_num_neighbors,
+      segmentation_smoothness_threshold, segmentation_curvature_threshold));
   rclcpp::shutdown();
   return 0;
 
